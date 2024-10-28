715a55ff9db31485fe278ad3425198f7ee898477
Dr-T <xandertang@qq.com>
2024-10-29 01:38:53 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ import { seo } from '~/lib/seo'
 export const metadata: Metadata = {
   metadataBase: seo.url,
   title: {
-    template: '%s | Cali Castle',
+    template: '%s | 安德',
     default: seo.title,
   },
   description: seo.description,
-  keywords: 'Cali,Cali Castle,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
+  keywords: '安德,xandertang,产品经理,细节控,完美主义,创新',
   manifest: '/site.webmanifest',
   robots: {
     index: true,
@@ -34,13 +34,13 @@ export const metadata: Metadata = {
   openGraph: {
     title: {
       default: seo.title,
-      template: '%s | Cali Castle',
+      template: '%s | 安德',
     },
     description: seo.description,
-    siteName: 'Cali Castle',
+    siteName: '安德',
     locale: 'zh_CN',
     type: 'website',
-    url: 'https://cali.so',
+    url: 'https://me.tx88.eu.org',
   },
   twitter: {
     site: '@thecalicastle',
