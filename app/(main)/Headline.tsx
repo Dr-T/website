757474e14a7d1e006a4b5853ee0152ca33f3331a
Dr-T <xandertang@qq.com>
2024-10-29 01:02:44 +0800

    Update 简介.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -10,7 +10,7 @@ import { SocialLink } from '~/components/links/SocialLink'
 function Developer() {
   return (
     <span className="group">
-      <span className="font-mono">&lt;</span>开发者
+      <span className="font-mono">&lt;</span>产品经理
       <span className="font-mono">/&gt;</span>
       <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
     </span>
@@ -80,10 +80,9 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Cali，
-          <PeekabooLink href="https://zolplay.com">佐玩</PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
+          我是安德，不知名产品经理，热爱设计、创新、享受生活，以及在未知领域中探索。
+          目前酷爱折腾各类 AI 产品，搭建了一个<PeekabooLink href="https://zolplay.com">公益 AI 项目</PeekabooLink>，
+          免费使用最先进的 AI 模型，包括 GPT-4o、Claude 3.5、Gemini Pro、llama3 等，让你的创意无限发挥。
         </Balancer>
       </motion.p>
       <motion.div
@@ -99,33 +98,33 @@ export function Headline() {
         }}
       >
         <SocialLink
-          href="https://cali.so/twitter"
+          href="https://x.com/tangxuanV"
           aria-label="我的推特"
           platform="twitter"
         />
         <SocialLink
-          href="https://cali.so/youtube"
+          href="https://www.youtube.com/@xuantangV"
           aria-label="我的 YouTube"
           platform="youtube"
         />
         <SocialLink
-          href="https://cali.so/bilibili"
+          href="https://space.bilibili.com/37729840"
           aria-label="我的 Bilibili"
           platform="bilibili"
         />
         <SocialLink
-          href="https://cali.so/github"
+          href="https://github.com/Dr-T"
           aria-label="我的 GitHub"
           platform="github"
         />
         <SocialLink
-          href="https://cali.so/tg"
+          href="https://t.me/xandertang"
           aria-label="我的 Telegram"
           platform="telegram"
         />
         <SocialLink href="/feed.xml" platform="rss" aria-label="RSS 订阅" />
         <SocialLink
-          href="mailto:hi@cali.so"
+          href="mailto:xandertang@qq.com"
           aria-label="我的邮箱"
           platform="mail"
         />
