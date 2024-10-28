463b5c522649d4a213625429a938048879c79ee6
Dr-T <xandertang@qq.com>
2024-10-29 01:19:11 +0800

    Update tab页签

--- config/nav.ts
+++ config/nav.ts
@@ -1,8 +1,9 @@
 export const navigationItems = [
   { href: '/', text: '首页' },
-  { href: '/blog', text: '博客' },
+  { href: '/blog', text: '笔记' },
   { href: '/projects', text: '项目' },
   { href: '/guestbook', text: '留言墙' },
-  { href: '/ama', text: 'AMA 咨询' },
+  { href: 'https://chatx.tx88.eu.org', text: '公益AI' },
+  // { href: '/ama', text: 'AMA 咨询' },
   // { href: '/about', text: '关于' },
 ]
